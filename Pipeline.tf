24bfed18f13838f32b0bc3c91f5692e68da13e91
Francisrega <francis.regalado1@gmail.com>
2023-05-10 00:30:53 -0400

    Began adding script to file

---
+++ Pipeline.tf
@@ -0,0 +1,20 @@
+#Define variables
+variable "compartment_ocid" {}
+variable "vcn_ocid" {}
+variable "subnet_ocid" {}
+variable "instance_display_name" {}
+variable "instance_shape" {}
+variable "instance_image_id" {}
+variable "instance_ssh_public_key" {}
+variable "instance_count" {}
+variable "git_repo_url" {}
+variable "git_repo_branch" {}
+variable "app_name" {}
+variable "fn_app_name" {}
+variable "fn_function_name" {}
+variable "fn_application_id" {}
+variable "fn_invoke_function_endpoint" {}
+variable "namespace" {}
+variable "bucket" {}
+variable "region" {}
+
