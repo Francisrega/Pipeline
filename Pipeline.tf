e08ef141347824f13c4695c331ac535017d20ec1
Francisrega <francis.regalado1@gmail.com>
2023-05-10 00:50:24 -0400

    added changes to the code

--- Pipeline.tf
+++ Pipeline.tf
@@ -18,3 +18,24 @@ variable "namespace" {}
 variable "bucket" {}
 variable "region" {}
 
+#Define resources
+resource "oci_core_instance" "devops_instance"{
+    availability_domain = "${var.availability_domain}"
+    compartment_id      = "${var.compartment_ocid}"
+    display_name        = "${var.instance_display_name}"
+    shape               = "${var.instance_shape}"
+    image_id            = "${var.instance_image_id}"
+    subnet_id           = "${var.subnet_ocid}"
+    metadata            = {
+        ssh_authorized_keys = "${var.instance_ssh_public_key}"
+  }
+  count               = "${var.instance_count}"
+}
+
+resource "oci_devops_pipeline" "devops_pipeline" {
+  compartment_id      = "${var.compartment_ocid}"
+  display_name        = "${var.app_name}"
+  defined_tags = {
+    "owner" = "devops_team"
+  }
+
